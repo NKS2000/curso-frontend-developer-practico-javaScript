8d3f4d58bc8f3098a23110c0cc3771749cf7ba13
alguien <nks.phasma@gmail.com>
2022-10-04 15:13:56 -0500

    Mobile menu added and fixed with desktop menu

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const navMenu = document.querySelector('.desktop-menu');
+const burgerMobileMen = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+burgerMobileMen.addEventListener('click', toogleMobileMenu);
+
 
 function toggleDesktopMenu(){
     navMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
