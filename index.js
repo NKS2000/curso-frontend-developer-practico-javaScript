d310ba111a1c2f50628c7e5c7416c74cf306c0e6
alguien <nks.phasma@gmail.com>
2022-10-05 12:48:31 -0500

    main content added,array creation of products and hotfix

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const burgerMobileMen = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cartNotif = document.querySelector('.navbar-shopping-cart');
 const cartAside = document.querySelector('.product-detail');
+const cardContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 burgerMobileMen.addEventListener('click', toogleMobileMenu);
@@ -39,3 +40,41 @@ function toggleCartAside(){
 
     cartAside.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: 'bike',
+    price: '120',
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name: 'Laptop',
+    price: '320',
+    image: 'https://images.pexels.com/photos/812264/pexels-photo-812264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+    name: 'chair',
+    price: '56',
+    image: 'https://images.pexels.com/photos/2762247/pexels-photo-2762247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCards = `<div class="product-card">
+        <img src= ${product.image} class="product-img">
+        <div class="product-info">
+          <div>
+            <p>${"$"+product.price}</p>
+            <p>${product.name}</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>`
+    
+        cardContainer.innerHTML += productCards;
+    }
+}
+renderProducts(productList);
+
